aa846a3b7b600138d0c65e5936ce8ae7ec7f0a8d
RockitSaucer <148157393+RockitSaucer@users.noreply.github.com>
2026-08-06 14:12:32 -0500

    V7.0.26: rename Walker icon to Dobbs (pin + GPS)

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * Caches app shell for return visits with no signal.
  * Map tiles: cache-first when already stored; network otherwise (then cache).
  */
-const SHELL_CACHE = 'reg-slayer-shell-v85';
+const SHELL_CACHE = 'reg-slayer-shell-v86';
 const TILE_CACHE = 'reg-slayer-tiles-v2';
 const DATA_CACHE = 'reg-slayer-data-v1';
 
@@ -61,7 +61,7 @@ const SHELL_ASSETS = [
   './icons/pins/tent.png',
   './icons/pins/tree.png',
   './icons/pins/truck.png',
-  './icons/pins/walker.png',
+  './icons/pins/dobbs.png',
   // Directional location icons (party / GPS)
   './icons/dir/arrow_head.png',
   './icons/dir/boat.png',
@@ -75,7 +75,7 @@ const SHELL_ASSETS = [
   './icons/dir/shuttle.png',
   './icons/dir/speed_boat.png',
   './icons/dir/truck.png',
-  './icons/dir/walker.png',
+  './icons/dir/dobbs.png',
   './icons/dir/x_wing.png',
   './vendor/leaflet/leaflet.js',
   './vendor/leaflet/leaflet.css',
